bd0e1783e84005dfdefc51920cf2757a3964c025
arnfinn <arnfinn.steindal@gmail.com>
2026-08-17 12:42:41 +0200

    Diverse docker-rydd
    - Dropp å dytt image til dockerhub.
    - Lag db for log og autoreport ved docker-compose-oppstart.
    - Bruk image som fungerer på arm i docker-compose (dropp platform).

--- dev/mysql/1_create_db.sql
+++ dev/mysql/1_create_db.sql
@@ -2,3 +2,54 @@ CREATE DATABASE IF NOT EXISTS `traume`
   DEFAULT CHARACTER SET utf8
   DEFAULT COLLATE utf8_danish_ci;
 
+CREATE DATABASE IF NOT EXISTS `db_autoreport`;
+
+USE `db_autoreport`;
+
+CREATE TABLE IF NOT EXISTS `autoreport` (
+  `id` varchar(255) COLLATE utf8mb4_danish_ci DEFAULT NULL,
+  `synopsis` varchar(255) COLLATE utf8mb4_danish_ci DEFAULT NULL,
+  `package` varchar(255) COLLATE utf8mb4_danish_ci DEFAULT NULL,
+  `fun` varchar(255) COLLATE utf8mb4_danish_ci DEFAULT NULL,
+  `params` text COLLATE utf8mb4_danish_ci,
+  `owner` varchar(255) COLLATE utf8mb4_danish_ci DEFAULT NULL,
+  `email` varchar(255) COLLATE utf8mb4_danish_ci DEFAULT NULL,
+  `organization` varchar(255) COLLATE utf8mb4_danish_ci DEFAULT NULL,
+  `terminateDate` varchar(255) COLLATE utf8mb4_danish_ci DEFAULT NULL,
+  `interval` varchar(255) COLLATE utf8mb4_danish_ci DEFAULT NULL,
+  `intervalName` varchar(255) COLLATE utf8mb4_danish_ci DEFAULT NULL,
+  `runDayOfYear` text COLLATE utf8mb4_danish_ci,
+  `type` varchar(255) COLLATE utf8mb4_danish_ci DEFAULT NULL,
+  `ownerName` varchar(255) COLLATE utf8mb4_danish_ci DEFAULT NULL,
+  `startDate` varchar(255) COLLATE utf8mb4_danish_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_danish_ci;
+
+CREATE DATABASE IF NOT EXISTS `db_log`;
+
+USE `db_log`;
+
+CREATE TABLE IF NOT EXISTS `appLog` (
+  `id` int(9) unsigned NOT NULL AUTO_INCREMENT,
+  `time` datetime DEFAULT NULL,
+  `user` varchar(127) COLLATE utf8_danish_ci DEFAULT NULL,
+  `name` varchar(255) COLLATE utf8_danish_ci DEFAULT NULL,
+  `group` varchar(63) COLLATE utf8_danish_ci DEFAULT NULL,
+  `role` varchar(63) COLLATE utf8_danish_ci DEFAULT NULL,
+  `resh_id` varchar(31) COLLATE utf8_danish_ci DEFAULT NULL,
+  `message` varchar(2047) COLLATE utf8_danish_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_danish_ci;
+
+CREATE TABLE IF NOT EXISTS `reportLog` (
+  `id` int(9) unsigned NOT NULL AUTO_INCREMENT,
+  `time` datetime DEFAULT NULL,
+  `user` varchar(127) COLLATE utf8_danish_ci DEFAULT NULL,
+  `name` varchar(255) COLLATE utf8_danish_ci DEFAULT NULL,
+  `group` varchar(63) COLLATE utf8_danish_ci DEFAULT NULL,
+  `role` varchar(63) COLLATE utf8_danish_ci DEFAULT NULL,
+  `resh_id` varchar(31) COLLATE utf8_danish_ci DEFAULT NULL,
+  `environment` varchar(63) COLLATE utf8_danish_ci DEFAULT NULL,
+  `call` varchar(2047) COLLATE utf8_danish_ci DEFAULT NULL,
+  `message` varchar(2047) COLLATE utf8_danish_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_danish_ci;
